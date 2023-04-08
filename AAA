286db425de94c59151050818b0cac296227d2754
rickdev001 <125653054+rickdev001@users.noreply.github.com>
2023-04-08 04:48:03 -0600

    Files Upload

---
+++ AAA
@@ -0,0 +1,8 @@
+Tech Stack;
+1.HTML5
+2.CSS3
+3.Javascript
+4.PHP
+5.Google Fonts
+6.SQL
+7.Boostrap 4
---
+++ connection.php
@@ -0,0 +1,6 @@
+<?php
+
+	$conn = mysqli_connect("localhost","root","","complaint system management") or die("data base not connected");
+
+
+?>
